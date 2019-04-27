d7b76c429d407dd166c49bc216e1db1371be76d6
Joshua Watson <josh.m.watson@gmail.com>
2019-04-26 22:57:14 -0500

    added player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private float _runSpeed = 0f;
+    [Range(0, .3f)]
+    [SerializeField]
+    private float _smoothing = .05f;
+
+    private Rigidbody2D _rigidBody;
+    private Vector3 _velocity = Vector3.zero;
+
+    private void Awake()
+    {
+        _rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Vector3 targetVelocity = new Vector3(Input.GetAxis("Horizontal") * _runSpeed, Input.GetAxis("Vertical") * _runSpeed, 0.0f);
+
+        _rigidBody.velocity = Vector3.SmoothDamp(_rigidBody.velocity, targetVelocity, ref _velocity, _smoothing);
+    }
+}
