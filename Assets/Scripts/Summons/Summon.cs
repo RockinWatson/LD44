0fbec7e6ebdaaed2b0dacd1c690894b4a187473d
Greg <halfskye@gmail.com>
2019-04-27 15:23:01 -0700

    Scripts: Stub in Summons base class

---
+++ Assets/Scripts/Summons/Summon.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Summon : MonoBehaviour {
+
+    public enum Type
+    {
+
+    };
+
+    [SerializeField]
+    private float _healthCost = 10f;
+
+
+}
