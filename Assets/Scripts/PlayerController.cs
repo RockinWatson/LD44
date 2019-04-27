58f684db865f5aa201e75fd6a29c1ef7f0675876
Greg <halfskye@gmail.com>
2019-04-27 02:06:13 -0700

    Scripts: PlayerController: Integrated preliinary Slide

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,25 @@ public class PlayerController : MonoBehaviour {
 
     [SerializeField]
     private float _runSpeed = 0f;
+    public float GetRunSpeed() { return _runSpeed; }
     [Range(0, .3f)]
     [SerializeField]
     private float _smoothing = .05f;
 
+    [SerializeField]
+    private float _slideForce = 30f;
+    private bool _slideGo = false;
+
+    [SerializeField]
+    private Vector2 _axisBias = Vector2.one;
+
+    [SerializeField]
+    private GameObject _slideTrail = null;
+
     private Rigidbody2D _rigidBody;
+    public Rigidbody2D GetRigidbody() { return _rigidBody; }
     private Vector3 _velocity = Vector3.zero;
-    Vector3 targetVelocity;
+    private Vector3 _targetVelocity = Vector3.zero;
 
     private void Awake()
     {
@@ -19,11 +31,48 @@ public class PlayerController : MonoBehaviour {
 
     private void Update()
     {
-        targetVelocity = new Vector3(Input.GetAxis("Horizontal") * _runSpeed, Input.GetAxis("Vertical") * _runSpeed, 0.0f);
+        //_targetVelocity = new Vector3(Input.GetAxis("Horizontal") * _runSpeed, Input.GetAxis("Vertical") * _runSpeed, 0.0f);
+        _targetVelocity.x = Input.GetAxis("Horizontal");
+        _targetVelocity.y = Input.GetAxis("Vertical");
+        _targetVelocity *= _runSpeed;
+
+        UpdatePlayerInput();
     }
 
     private void FixedUpdate()
     {
-        _rigidBody.velocity = Vector3.SmoothDamp(_rigidBody.velocity, targetVelocity, ref _velocity, _smoothing);
+        _rigidBody.velocity = Vector3.SmoothDamp(_rigidBody.velocity, _targetVelocity, ref _velocity, _smoothing) * Time.fixedDeltaTime;
+
+        //if(_slideGo)
+        //{
+        //    Slide();
+        //    _slideGo = false;
+        //}
+    }
+
+    private void UpdatePlayerInput()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            //_slideGo = true;
+            Slide();
+
+            //@TODO: Pump the breaks with E
+            //@TODO: Maybe check state of slide, if actively going, bum
+        }
+    }
+
+    private void Slide()
+    {
+        //@TODO: Based on directional aim, slide/skate player
+        //Vector3 dir = new Vector3(Input.GetAxis("Horizontal") * _slideForce, Input.GetAxis("Vertical") * _slideForce, 0.0f);
+
+        Vector3 dir = new Vector3(Input.GetAxis("Horizontal") * _axisBias.x, Input.GetAxis("Vertical") * _axisBias.y, 0.0f);
+        _rigidBody.AddForceAtPosition(dir * _slideForce, _rigidBody.position, ForceMode2D.Impulse);
+
+        //@TODO: Slide Trail setup
+        GameObject go = Instantiate(_slideTrail);
+        SlideTrail trail = go.GetComponent<SlideTrail>(); // (SlideTrail)Instantiate(_slideTrail);
+        trail.Initialize(this);
+
     }
 }
