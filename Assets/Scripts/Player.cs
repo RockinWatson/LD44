d4bcf10af258be2bd50db72e60268b2516af3772
Greg <halfskye@gmail.com>
2019-04-27 15:23:17 -0700

    Scripts: Add Player script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    static private Player _player = null;
+    static public Player Get() { return _player; }
+
+    [SerializeField]
+    private float _healthStart = 100f;
+    private float _health = 100f;
+
+    private void Awake()
+    {
+        _player = this;
+
+        _health = _healthStart;
+    }
+}
