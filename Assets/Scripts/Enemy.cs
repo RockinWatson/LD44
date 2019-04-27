19200fe38f21891a57c5dd28d55baaaafa967123
Joshua Watson <josh.m.watson@gmail.com>
2019-04-27 16:17:08 -0500

    Added Enemy spawners

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Enemy : MonoBehaviour
+    {
+        public float speed;
+
+        public GameObject player;
+
+        //TODO: Enemy Reset
+
+        private void FixedUpdate()
+        {
+            var range = Vector2.Distance(transform.position, player.transform.position);
+            transform.Translate(Vector2.MoveTowards(transform.position, player.transform.position, range) * speed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class EnemySpawner : MonoBehaviour
+    {
+        public GameObject[] Objects;
+        public int ObjectPoolSize;
+        public float WaitForNextMax;
+        public float WaitForNextMin;
+        public float CountDown;
+
+        public Sprite[] enemySprites;
+        //public RuntimeAnimatorController[] enemyAnimations;
+
+        private List<GameObject> _objectsPool;
+        private Transform _spawnerTrans;
+
+        // Use this for initialization
+        void Start()
+        {
+            _spawnerTrans = GetComponent<Transform>();
+            _objectsPool = new List<GameObject>();
+            foreach (var obj in Objects)
+            {
+                for (int i = 0; i < ObjectPoolSize; i++)
+                {
+                    GameObject o = Instantiate(obj);
+                    o.SetActive(false);
+                    _objectsPool.Add(o);
+                }
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            CountDown -= Time.deltaTime;
+            if (CountDown <= 0)
+            {
+                SpawnRandObj(_objectsPool, _spawnerTrans);
+                CountDown = Random.Range(WaitForNextMin, WaitForNextMax);
+            }
+        }
+
+        private void SpawnRandObj(List<GameObject> objPool, Transform transform)
+        {
+            if (objPool != null)
+            {
+                GameObject gameObj = objPool[Random.Range(0, objPool.Count)];
+                if (!gameObj.activeInHierarchy)
+                {
+                    InstantiateObject(gameObj, transform);
+                }
+            }
+        }
+
+        private void InstantiateObject(GameObject gameObj, Transform transform)
+        {
+            Vector2 pos;
+            pos = new Vector2(transform.position.x, transform.position.y);
+            gameObj.transform.position = pos;
+
+            AssignRandomSpriteAnimation(gameObj);
+
+            //TODO: Reset Enemy
+
+            gameObj.SetActive(true);
+        }
+
+        private void AssignRandomSpriteAnimation(GameObject gameObj) {
+            SpriteRenderer sprRend = gameObj.GetComponent<SpriteRenderer>();
+            Animator animator = gameObj.GetComponent<Animator>();
+
+            int getInt = Random.Range(0, enemySprites.Length);
+
+            Sprite randSprite = enemySprites[getInt];
+            sprRend.sprite = randSprite;
+            //RuntimeAnimatorController animationController = enemyAnimations[getInt];
+            //animator.runtimeAnimatorController = animationController;
+        }
+    }
+}
