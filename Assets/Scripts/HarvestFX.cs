4f0cf3d276123e4ad9eb9414c062afbd02c22587
Greg <halfskye@gmail.com>
2019-04-27 17:11:12 -0700

    Scripts: Add HarvestFX functionality

---
+++ Assets/Scripts/HarvestFX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HarvestFX : MonoBehaviour {
+
+    [SerializeField]
+    private float _lifetime = 1.5f;
+    private float _lifeTimer = 0.0f;
+
+    private void Awake()
+    {
+        _lifeTimer = _lifetime;
+    }
+
+    private void Update()
+    {
+        _lifeTimer -= Time.deltaTime;
+        if(_lifeTimer < 0f)
+        {
+            Destroy(this.transform.parent.gameObject);
+        }
+    }
+}
