b75ee460761260ace2240c7547c42297762ba7cf
Greg <halfskye@gmail.com>
2019-04-27 22:21:03 -0700

    Scripts: Corpse & HarvestFX lifetime alphas

--- Assets/Scripts/Corpse.cs
+++ Assets/Scripts/Corpse.cs
@@ -6,23 +6,75 @@ public class Corpse : MonoBehaviour {
 
     [SerializeField]
     private GameObject _harvestFX = null;
+    private HarvestFX _FX = null;
 
     [SerializeField]
     private float _harvestValue = 30f;
 
     private bool _isHarvested = false;
     public bool IsHarvested() { return _isHarvested; }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    [SerializeField]
+    private float _lifetime = 5.0f;
+    private float _lifeTimer = 0.0f;
+    [SerializeField]
+    private float _deathTime = 3.0f;
+    private float _deathTimer = 0.0f;
+
+    private SpriteRenderer _renderer = null;
+
+    private void Awake()
+    {
+        _renderer = this.GetComponent<SpriteRenderer>();
+        _lifeTimer = _lifetime;
+        _deathTimer = _deathTime;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (_isHarvested)
+        {
+            UpdateDeathState();
+        } else
+        {
+            _lifeTimer -= Time.deltaTime;
+            if (_lifeTimer < 0f)
+            {
+                UpdateDeathState();
+            }
+        }
+    }
+
+    private void UpdateDeathState()
+    {
+        _deathTimer -= Time.deltaTime;
+        if (_deathTimer < 0f)
+        {
+            Destroy(this.transform.gameObject);
+        }
+
+        float scale = _deathTimer / _deathTime;
+        Color color = _renderer.color;
+        color.a = scale;
+        _renderer.color = color;
+
+        if(_FX)
+        {
+            _FX.UpdateAlpha(scale);
+        }
+    }
 
     public float Harvest()
     {
         _isHarvested = true;
+        _deathTimer = _deathTime;
 
-        Instantiate(_harvestFX, this.transform);
+        GameObject go = Instantiate(_harvestFX, this.transform);
+        _FX = go.GetComponent<HarvestFX>();
+
+        Color color = _renderer.color;
+        color.a = 1.0f;
+        _renderer.color = color;
 
         return _harvestValue;
     }
--- Assets/Scripts/HarvestFX.cs
+++ Assets/Scripts/HarvestFX.cs
@@ -8,9 +8,17 @@ public class HarvestFX : MonoBehaviour {
     private float _lifetime = 1.5f;
     private float _lifeTimer = 0.0f;
 
+    [SerializeField]
+    private float _harvestedLifetime = 5.0f;
+    private float _harvestedTimer = 0.0f;
+
+    private SpriteRenderer _renderer = null;
+
     private void Awake()
     {
+        _renderer = this.GetComponent<SpriteRenderer>();
         _lifeTimer = _lifetime;
+        _harvestedTimer = _harvestedLifetime;
     }
 
     private void Update()
@@ -18,7 +26,25 @@ public class HarvestFX : MonoBehaviour {
         _lifeTimer -= Time.deltaTime;
         if(_lifeTimer < 0f)
         {
+            //UpdateDeathState();
+        }
+    }
+
+    private void UpdateDeathState()
+    {
+        _harvestedTimer -= Time.deltaTime;
+        if (_harvestedTimer < 0f)
+        {
             Destroy(this.transform.parent.gameObject);
         }
+
+        UpdateAlpha(_harvestedTimer / _harvestedLifetime);
+    }
+
+    public void UpdateAlpha(float alpha)
+    {
+        Color color = _renderer.color;
+        color.a = alpha;
+        _renderer.color = color;
     }
 }
