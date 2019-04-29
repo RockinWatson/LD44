3634798bc47401e1a71f2f98c07de5e7e917d754
Greg <halfskye@gmail.com>
2019-04-29 14:25:54 -0700

    Scripts: Summons: Lightning: Changed behavior to attack nearby enemies w/ FX

--- Assets/Scripts/Summons/Lightning.cs
+++ Assets/Scripts/Summons/Lightning.cs
@@ -3,4 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Lightning : Summon {
+    [SerializeField]
+    private GameObject _FX = null;
+
+    [SerializeField]
+    private float _effectRadius = 2f;
+
+    [SerializeField]
+    private float _stunTime = 2f;
+
+    private RaycastHit2D[] _hits = new RaycastHit2D[10];
+
+    private void Start()
+    {
+        Attack();
+    }
+
+    private void Attack()
+    {
+        int count = Physics2D.CircleCast(this.transform.position, _effectRadius, Vector2.zero, new ContactFilter2D(), _hits);
+        if (count > 0)
+        {
+            foreach (RaycastHit2D hit in _hits)
+            {
+                if (hit && hit.transform)
+                {
+                    Enemy enemy = hit.transform.GetComponent<Enemy>();
+                    if (enemy && enemy.IsEnabled())
+                    {
+                        enemy.Stun(_stunTime);
+
+                        GameObject go = Instantiate(_FX, enemy.transform);
+                        Destroy(go, _stunTime);
+                    }
+                }
+            }
+        }
+    }
 }
