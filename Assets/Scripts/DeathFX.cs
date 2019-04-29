58117fc8a8ab99d3d1a3cec798ff6a48522b48c7
Greg <halfskye@gmail.com>
2019-04-29 15:24:40 -0700

    Scripts: DeathFX functionality

---
+++ Assets/Scripts/DeathFX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathFX : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject _corpse = null;
+
+    public void Corpse()
+    {
+        SpawnCorpse();
+
+        Destroy(this.gameObject);
+    }
+
+    private void SpawnCorpse()
+    {
+        if (_corpse)
+        {
+            Instantiate(_corpse, this.transform.position, Quaternion.identity);
+        }
+    }
+}
