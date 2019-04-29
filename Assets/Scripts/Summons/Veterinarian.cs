dd68bcade7e97543aff1741f12266bcfea0fadf0
Greg <halfskye@gmail.com>
2019-04-29 00:12:45 -0700

    Scripts: Veterinarian: Add Attack function

--- Assets/Scripts/Summons/Veterinarian.cs
+++ Assets/Scripts/Summons/Veterinarian.cs
@@ -3,4 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Veterinarian : PursuerSummon {
+
+    public void Attack()
+    {
+        Enemy target = GetTarget();
+        if (!target || !target.gameObject.activeSelf) return;
+        if((target.transform.position - this.transform.position).sqrMagnitude < 1f)
+        {
+            target.Kill();
+        }
+    }
 }
