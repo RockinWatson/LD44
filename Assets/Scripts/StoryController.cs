e535b85ae88031834ac757c6dfed9eab585ed5a3
Stephen Roper <stephenroper@me.com>
2019-04-28 22:19:50 -0500

    Updates story transitions.

--- Assets/Scripts/StoryController.cs
+++ Assets/Scripts/StoryController.cs
@@ -12,12 +12,16 @@ public class StoryController : MonoBehaviour {
     public AudioSource select;
 
     private Vector3 cardPos;
-    public GameObject card2;
-    public GameObject card3;
-    public GameObject card4;
-    public GameObject card5;
-    public GameObject card6;
-    public GameObject card7;
+    private GameObject card2;
+    private GameObject card3;
+    private GameObject card4;
+    private GameObject card5;
+    private GameObject card6;
+    private GameObject card7;
+    private GameObject card8;
+    private GameObject card9;
+    private GameObject card10;
+    private GameObject card11;
 
     private float selectVolume;
 
@@ -33,6 +37,10 @@ public class StoryController : MonoBehaviour {
         card5 = GameObject.Find("Card5");
         card6 = GameObject.Find("Card6");
         card7 = GameObject.Find("Card7");
+        card8 = GameObject.Find("Card8");
+        card9 = GameObject.Find("Card9");
+        card10 = GameObject.Find("Card10");
+        card11 = GameObject.Find("Card11");
 
         isStory = true;
         InitAudio();
@@ -74,27 +82,43 @@ public class StoryController : MonoBehaviour {
         {
             if (card2.transform.position.x != cardPos.x)
             {
-                card2.transform.position = new Vector3(0, 0, -2);
+                card2.transform.position = cardPos;
             }
             else if (card3.transform.position.x != cardPos.x)
             {
-                card3.transform.position = new Vector3(0, 0, -3);
+                card3.transform.position = cardPos;
             }
             else if (card4.transform.position.x != cardPos.x)
             {
-                card4.transform.position = new Vector3(0, 0, -4);
+                card4.transform.position = cardPos;
             }
             else if (card5.transform.position.x != cardPos.x)
             {
-                card5.transform.position = new Vector3(0, 0, -5);
+                card5.transform.position = cardPos;
             }
             else if (card6.transform.position.x != cardPos.x)
             {
-                card6.transform.position = new Vector3(0, 0, -6);
+                card6.transform.position = cardPos;
             }
             else if (card7.transform.position.x != cardPos.x)
             {
-                card7.transform.position = new Vector3(0, 0, -7);
+                card7.transform.position = cardPos;
+            }
+            else if (card8.transform.position.x != cardPos.x)
+            {
+                card8.transform.position = cardPos;
+            }
+            else if (card9.transform.position.x != cardPos.x)
+            {
+                card9.transform.position = cardPos;
+            }
+            else if (card10.transform.position.x != cardPos.x)
+            {
+                card10.transform.position = cardPos;
+            }
+            else if (card11.transform.position.x != cardPos.x)
+            {
+                card11.transform.position = cardPos;
             }
         }
     }
