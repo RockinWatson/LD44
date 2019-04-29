14edbce57d130ee3be5c5ea746959a882dd66a2c
Joshua Watson <josh.m.watson@gmail.com>
2019-04-29 17:00:24 -0500

    Added Blood Level Controller hook up to Health and made smooth movement.

--- Assets/Scripts/BloodController.cs
+++ Assets/Scripts/BloodController.cs
@@ -1,17 +1,19 @@
 ﻿using UnityEngine;
 
 public class BloodController : MonoBehaviour {
+
     public GameObject BloodLevel;
     private Vector3 _bloodLevelVisualStartPos;
     private Vector3 _bloodLevelVisualZeroPos;
-    private float _bloodLevelMaxForVisual = 1000.0f;
+    private float _bloodLevelMaxForVisual = 9000f;
     private float _bloodLevelMaxPosX = 0.26f;
+    private float _bloodLevelMinPosX = -12.34f;
 
     // Use this for initialization
     void Start()
     {
         float scaleToZero = Player.Get().GetHealth() / _bloodLevelMaxForVisual * _bloodLevelMaxPosX;
-        _bloodLevelVisualZeroPos = BloodLevel.transform.position - (Vector3.left * scaleToZero);
+        _bloodLevelVisualZeroPos = BloodLevel.transform.position - (Vector3.right * scaleToZero);
     }
 
     // Update is called once per frame
@@ -23,6 +25,8 @@ public class BloodController : MonoBehaviour {
     private void UpdateBloodLevelVisual()
     {
         float bloodScale = Mathf.Min(Player.Get().GetHealth(), _bloodLevelMaxForVisual) / _bloodLevelMaxForVisual * _bloodLevelMaxPosX;
-        BloodLevel.transform.position = _bloodLevelVisualZeroPos + (Vector3.left * bloodScale);
+        
+        BloodLevel.transform.position = Vector3.Lerp(BloodLevel.transform.position, _bloodLevelVisualZeroPos + (Vector3.right * bloodScale), Time.deltaTime * 5);
+        //Mathf.Lerp(_bloodLevelMaxPosX,_bloodLevelMinPosX, Vector3.right * bloodScale);
     }
 }
