6cd207304574e52c06b6306d5b8c4fbfe2d8ca90
Greg <halfskye@gmail.com>
2019-04-29 17:50:58 -0700

    Scripts: BloodController: Positioning fixes

--- Assets/Scripts/BloodController.cs
+++ Assets/Scripts/BloodController.cs
@@ -7,14 +7,15 @@ public class BloodController : MonoBehaviour {
     private Vector3 _bloodLevelVisualStartPos;
     private Vector3 _bloodLevelVisualZeroPos;
     private float _bloodLevelMaxForVisual = 9000f;
-    private float _bloodLevelMaxPosX = 0.26f;
-    private float _bloodLevelMinPosX = -12.34f;
+    private float _bloodLevelMaxPosX = 0.01f;
+    private float _bloodLevelMinPosX = -12.74f;
 
     // Use this for initialization
     void Start()
     {
-        float scaleToZero = Player.Get().GetHealth() / _bloodLevelMaxForVisual * _bloodLevelMaxPosX;
-        _bloodLevelVisualZeroPos = BloodLevel.transform.position - (Vector3.right * scaleToZero);
+        float scaleToZero = Player.Get().GetHealth() / _bloodLevelMaxForVisual * (_bloodLevelMaxPosX - _bloodLevelMinPosX);
+        _bloodLevelVisualZeroPos = BloodLevel.transform.position;
+        _bloodLevelVisualZeroPos.x = _bloodLevelMinPosX;
     }
 
     // Update is called once per frame
@@ -32,7 +33,7 @@ public class BloodController : MonoBehaviour {
 
     private void UpdateBloodLevelVisual()
     {
-        float bloodScale = Mathf.Min(Player.Get().GetHealth(), _bloodLevelMaxForVisual) / _bloodLevelMaxForVisual * _bloodLevelMaxPosX;
+        float bloodScale = Mathf.Min(Player.Get().GetHealth(), _bloodLevelMaxForVisual) / _bloodLevelMaxForVisual * (_bloodLevelMaxPosX - _bloodLevelMinPosX);
         
         BloodLevel.transform.position = Vector3.Lerp(BloodLevel.transform.position, _bloodLevelVisualZeroPos + (Vector3.right * bloodScale), Time.deltaTime * 5);
         //Mathf.Lerp(_bloodLevelMaxPosX,_bloodLevelMinPosX, Vector3.right * bloodScale);
